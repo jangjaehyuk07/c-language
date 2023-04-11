1cf4e77453a4cc55942fc2970b80b9fb97d264bf
재혁 <gurl0414@naver.com>
2023-04-11 06:59:21 +0000

    adad

--- 1.55.c
+++ 1.55.c
@@ -4,6 +4,14 @@ main()
 {
 	int a = 3;
 	a = a + 6;
-	if(a>5) printf("%d\n", a);
-	//printf("d\n", a-6);
+	
+	if(a>5) {
+		printf("%d\n", a);
+		printf("조건이 참일을 때, 실행 \n");
+		
+	}else {
+		printf("%d\n", a);
+		printf("조건이 거짓일을 때, 실행 \n");
+	}
+	
 }
