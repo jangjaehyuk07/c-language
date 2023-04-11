98e34c19e8d0edd795729ff6a83a5b0a74d2f0c3
재혁 <gurl0414@naver.com>
2023-04-11 07:09:10 +0000

    123

---
+++ 55-2.c
@@ -0,0 +1,17 @@
+
+
+main()
+{
+	int a = 3;
+	a = a + 6;
+	
+	if(a>5) {
+		printf("%d\n", a);
+		printf("조건이 참일을 때, 실행 \n");
+		
+	}else {
+		printf("%d\n", a);
+		printf("조건이 거짓일을 때, 실행 \n");
+	}
+	
+}
